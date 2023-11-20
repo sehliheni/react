14fa64e7b15c2b9b516539e4bdb2d0ef40a38d6e
sehly heny <sehli.sehli.heni@gmail.com>
2023-11-20 16:29:13 +0100

    react done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App (){
+return(
+    <div className="App" >
+        <header className="App-header">
+        <a href="index.html" className="title">portfolio</a>
+        <a href="Skills.js">skills</a>
+        <a href="contact.html">contact</a>
+        <a href="main.html">MAIN</a>
+        </header>
+
+    </div>
+
+);
+}
+export default App;
---
+++ src/Skills.js
@@ -0,0 +1,12 @@
+import'./App.css';
+
+
+function Skills(){
+    return(
+        
+<h2>heny sehly</h2>
+
+
+);
+}
+export default Skills;
